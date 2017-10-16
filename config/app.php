da13e71bb0c8acff54139b4a3212e183847c7e5f
yxk <75605042@qq.com>
2017-10-16 22:04:56 +0800

    add laraoole

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
 
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Laravoole\LaravooleServiceProvider::class,
 
     ],
 
